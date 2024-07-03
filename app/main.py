029776ce28857244ee097feda7f76077d99352d7
username-kunalg <kunalg.tracencode@gmail.com>
2024-07-03 15:09:06 +0530

    first commit

--- app/main.py
+++ app/main.py
@@ -45,6 +45,8 @@ async def register_page(request: Request):
     return templates.TemplateResponse("register.html", {"request": request})
 
 
+
+
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(
         plain_password.encode("utf-8"), hashed_password.encode("utf-8")
